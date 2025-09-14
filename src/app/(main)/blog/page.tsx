637a20cac82a90b9484a6060466940284abd7067
Vella Bro <maf415415@gmail.com>
2025-09-14 20:48:08 +0000

    till xl screen show 5 cards for blog, till lg screen show 4 and till md

--- src/app/(main)/blog/page.tsx
+++ src/app/(main)/blog/page.tsx
@@ -133,7 +133,7 @@ export default async function BlogPage(props: { searchParams: Promise<SearchPara
             <p>Try adjusting your search or filters. Or check back soon!</p>
           </div>
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
             {blogPosts.map((post, index) => {
               const readTime = Math.ceil(post.content.split(/\s+/).length / 200);
               return (
