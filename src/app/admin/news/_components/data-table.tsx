3fc63e280a49074c9211fb878643f8c62620eacd
Vella Bro <maf415415@gmail.com>
2025-10-12 09:39:45 +0000

    Invalid `prisma.newsArticle.findMany()` invocation:
    {
      where: {
        ti

--- src/app/admin/news/_components/data-table.tsx
+++ src/app/admin/news/_components/data-table.tsx
@@ -25,7 +25,6 @@ async function getArticles({ search, sort, page, per_page }: NewsArticlesDataTab
       where = {
           title: {
               contains: search,
-              mode: 'insensitive'
           }
       }
   }
