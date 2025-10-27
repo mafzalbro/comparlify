a28e43438ce29dc9f4f9ef985fb0ff05b34db8b3
Vella Bro <maf415415@gmail.com>
2025-10-27 13:44:10 +0000

    add all theme variables properties that was in css global.css file

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div className="relative flex flex-1 flex-col items-center justify-center text-center px-4 overflow-hidden">
-        <div className="absolute inset-0 bg-grid-pattern-light dark:bg-grid-pattern-dark [mask-image:linear-gradient(0deg,transparent,black)]"></div>
+        <div className="absolute inset-0 bg-grid-slate-900/[0.04] dark:bg-grid-slate-400/[0.05] [mask-image:linear-gradient(0deg,transparent,black)]"></div>
         <div className="relative flex flex-col items-center justify-center">
             <h1 className="text-[12rem] font-black font-headline leading-none text-transparent bg-clip-text bg-gradient-to-br from-primary/70 to-primary">
                 404
